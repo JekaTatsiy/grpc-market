1fcaa87bf04bc899d2bc114c8117e4c8527a9cdf
JekaTatsiy <jeka05.09.2002@yandex.ru>
2022-06-28 13:51:52 +0300

    init grpc server

--- search/main.go
+++ search/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"net"
 
-	"github.com/gorilla/mux"
+	"google.golang.org/grpc"
+
+	pb "github.com/JekaTatsiy/grpc-market/suggest_proto"
 )
+
+type server struct {
+	pb.UnimplementedSuggestServiceServer
+}
+
+func (s *server) Search(ctx context.Context, in *pb.SearchRequest) (*pb.SearchResponse, error) {
+	responses := make([]*pb.SuggestResponse, 0)
+	responses = append(responses, &pb.SuggestResponse{ID: 1, LinkUrl: "l", Title: "t", Queries: []string{"a", "b"}, Active: true})
+	return &pb.SearchResponse{Suggests: responses}, nil
+}
 
 func main() {
 
-	fmt.Println("search")
+	lis, err := net.Listen("tcp", "1000")
+	if err != nil {
+		fmt.Printf("failed to listen: %v\n", err)
+		return
+	}
 
-	r := mux.NewRouter()
-	server := http.Server{Addr: "0.0.0.0:1000", Handler: r}
-	server.ListenAndServe()
+	s := grpc.NewServer()
+	pb.RegisterSuggestServiceServer(s, &server{})
+	fmt.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		fmt.Printf("failed to serve: %v\n", err)
+		return
+	}
 }
